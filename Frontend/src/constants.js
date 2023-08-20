b42ad1a3503f082a8f13f0b93e0f6094b5e8619c
Jeetendra Prasad <jeetendraprasad@users.noreply.github.com>
2023-08-20 15:13:45 +0530

    changes done: added static ip address

--- Frontend/src/constants.js
+++ Frontend/src/constants.js
@@ -1,7 +1,7 @@
 
 const constants = {
-    //appUrl: "http://localhost:4000" // NOTE: No trailing slash (/)
-	appUrl: "http://35.200.216.204:4000" // NOTE: No trailing slash (/) And this will change evry time.
+    //appUrl: "http://localhost:4000" // NOTE: No trailing slash (/). This is for local deployment
+	appUrl: "http://35.207.232.125:4000" // NOTE: No trailing slash (/) And this will change evry time. This is for cloud deployment
 };
 
 export default constants;
